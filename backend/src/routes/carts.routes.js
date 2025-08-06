9e99d1e979a9387f7c04a98e6532be128a89bffa
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-08-06 12:08:42 +0530

    cart routes created

--- backend/src/routes/carts.routes.js
+++ backend/src/routes/carts.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.moddlewares.js";
 
 const cartsRouter = Router()
+
+cartsRouter.route("/get-cart").get(verifyJWT, getCart)
+cartsRouter.route("/get-cart-items").get(verifyJWT, getCartItems)
+cartsRouter.route("/add-items-to-cart").get(verifyJWT, addItemsToCart)
+cartsRouter.route("/remove-items-to-cart").get(verifyJWT, removeItemsToCart)
+
 
 export default cartsRouter
